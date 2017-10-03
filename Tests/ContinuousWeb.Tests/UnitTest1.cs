5bee2b78438f6407c6d052d3033389fa154efd0e
Lundin <tuba-ocak.lundin@sogeti.com>
2017-10-03 21:23:03 +0200

    unittest

---
+++ Tests/ContinuousWeb.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContinuousWeb.Controllers;
+
+namespace ContinuousWeb.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {        
+        [TestMethod]
+        public void AboutPageTest()
+        {
+            var homeController = new HomeController();
+            var result = homeController.About();
+        }
+    }
+}
