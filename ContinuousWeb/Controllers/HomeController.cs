b33bd09b01b0d161ad6b91f4c87cdeeaf2033e8e
Lundin <tuba-ocak.lundin@sogeti.com>
2017-10-04 10:33:53 +0200

    failcorrected

--- ContinuousWeb/Controllers/HomeController.cs
+++ ContinuousWeb/Controllers/HomeController.cs
@@ -23,10 +23,9 @@ namespace ContinuousWeb.Controllers
         }
 public IActionResult About()
 {
-    throw new NotImplementedException();
 
-    //ViewData["Message"] = "Your application description page.";
-    //return View();
+    ViewData["Message"] = "Your application description page.";
+    return View();
 }
         public IActionResult Error()
         {
